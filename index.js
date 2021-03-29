76234d8bcd05f62e1be88d6cc007a7141ccfa654
Francisco Castillo <Francisco21castillo@aol.com>
2021-03-29 17:24:51 -0400

    completed

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+let bf = document.getElementById('main');
+bf.remove();
+let newHeader= document.createElement('h1')
+newHeader.setAttribute('id','victory') 
+let b = document.createTextNode('Francisco is the champion ')
